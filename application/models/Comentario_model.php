0869e1f188cf0b71cf6be381ec9ccd114601a74b
Luis Mendoza <desarrollo@dxt.com.mx>
2023-11-06 09:12:48 -0600

    endpints del nuevo crm, faltan los de cotizaciones

--- application/models/Comentario_model.php
+++ application/models/Comentario_model.php
@@ -30,4 +30,32 @@ class Comentario_model extends CI_Model
         $this->db->insert($this->tabla, $data);
         return $this->db->insert_id();
     }
+
+    public function getComentariosForLead($id_lead)
+    {
+        $this->db->select("comentarios.*,comentario_tipo.id as id_tipo_comentario,comentario_tipo.icono as tipo_comentario, u.nombre autor");
+        $this->db->from($this->tabla);
+        $this->db->join("comentario_tipo", "comentario_tipo.id = comentarios.id_comentario_tipo");
+        $this->db->join("usuarios u", "u.id = comentarios.id_usuario");
+        $this->db->where("id_lead", $id_lead);
+        $this->db->order_by("fecha", "desc");
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function crearComentario($comentario, $isArchive, $fileInfo)
+    {
+        $comentarioInfo = array("id_empresa" => 1, "id_usuario" => $comentario["usuario_id"], "id_comentario_tipo" => $comentario["tipocomentario"], "comentario" => $comentario["comentario"], "id_lead" => $comentario["id_lead"]);
+        if (intval($isArchive) == 1) {
+            $comentarioInfo["url"] = $_SERVER["URL_RELATIVE_PATH"] . $fileInfo["nombre_archivo"];
+            $comentarioInfo["nombre_archivo"] = $fileInfo["nombre_archivo"];
+            $comentarioInfo["extension"] = $fileInfo["extension"];
+            $this->db->insert("comentarios", $comentarioInfo);
+            $id = $this->db->insert_id();
+        } else {
+            $this->db->insert("comentarios", $comentarioInfo);
+            $id = $this->db->insert_id();
+        }
+        return $id;
+    }
 }
